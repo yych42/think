681f72cbdc6cea089978f54852301ed91920accf
Allen <yaoyuchen@eml.cc>
2018-11-04 00:55:05 -0700

    Initial Commit

---
+++ Think/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Think
+//
+//  Created by Allen on 11/4/18.
+//  Copyright © 2018 Yaoyu Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
